580bd4dc7d19bab76f1d7e9121ba9dda27de0731
surajbshinde <surajbshinde@gmail.com>
2025-07-24 09:50:22 +0530

    tags added

--- main.tf
+++ main.tf
@@ -5,10 +5,11 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0cbbe2c6a1bb2ad63" # Example AMI ID, replace with a valid one
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "ExampleInstance"
+	Env = "Development"
   }
   
 }
